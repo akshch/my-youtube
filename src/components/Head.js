ed12b5e50a4eb3be26debbe2353104692aeb739b
akshch <akshaych2786@gmail.com>
2024-07-07 23:13:27 +0530

    Integrate advance search with external api, debouncing  and caching

--- src/components/Head.js
+++ src/components/Head.js
@@ -1,15 +1,56 @@
-import React from "react";
-import { useDispatch } from "react-redux";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { toogleMenu } from "../utils/appSlice";
+import { YOUTUBE_SEARCH_API } from "../utils/constants";
+import { cacheResult } from "../utils/searchSlice";
 
 const Head = () => {
+  const [searchQuery, setSearchQuery] = useState("");
+  const [suggestions, setSuggestions] = useState([]);
+  const [showSuggestions, setShowSuggestions] = useState(false);
+
+  const searchCache = useSelector((store) => store.search);
+
+  useEffect(() => {
+    // API call
+    const timer = setTimeout(() => {
+      if (searchCache[searchQuery]) {
+        setSuggestions(searchCache[searchQuery]);
+      } else {
+        getSearchSuggestions();
+      }
+    }, 200);
+
+    return () => {
+      clearTimeout(timer);
+    };
+
+    // make an API call after every key press
+    // but if the difference between 2 Api calls is < 200ms
+    // decline the API call
+  }, [searchQuery]);
+
+  const getSearchSuggestions = async () => {
+    console.log("API Call-", searchQuery);
+    const data = await fetch(YOUTUBE_SEARCH_API + searchQuery);
+    const json = await data.json();
+    setSuggestions(json[1]);
+    // update the cache
+    dispatch(
+      cacheResult({
+        [searchQuery]: json[1],
+      })
+    );
+  };
+
   const dispatch = useDispatch();
 
   const toogleMenuHandler = () => {
     dispatch(toogleMenu());
   };
+
   return (
-    <div className="grid grid-flow-col p-5 m-2 shadow-lg cursor-pointer">
+    <div className="grid grid-flow-col p-5 m-2 shadow-lg">
       <div className="flex col-span-1">
         <img
           onClick={() => toogleMenuHandler()}
@@ -26,13 +67,30 @@ const Head = () => {
         </a>
       </div>
       <div className="col-span-10 px-10">
-        <input
-          className="w-1/2 border border-gray-400 p-2 rounded-l-full"
-          type="text"
-        ></input>
-        <button className="border border-gray-400 p-2 rounded-r-full bg-gray-400">
-          Search
-        </button>
+        <div>
+          <input
+            className="px-5 w-1/2 border border-gray-400 p-2 rounded-l-full"
+            type="text"
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+            onFocus={() => setShowSuggestions(true)}
+            onBlur={() => setShowSuggestions(false)}
+          ></input>
+          <button className="border border-gray-400 p-2 rounded-r-full bg-gray-400">
+            Search
+          </button>
+        </div>
+        <div className="absolute bg-white px-2 py-2 w-[48rem] shadow-lg rounded-lg border border-gray-100">
+          {showSuggestions && (
+            <ul>
+              {suggestions.map((s) => (
+                <li key={s} className="py-2 px-3 shadow-sm hover:bg-gray-100">
+                  {s}
+                </li>
+              ))}
+            </ul>
+          )}
+        </div>
       </div>
       <div className="col-span-1">
         <img
